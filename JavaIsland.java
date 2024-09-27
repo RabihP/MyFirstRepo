e8a21844029e7f47ddaac5a891cab1942a6c27a4
Rabih Perkasa <rabihperkasa04@gmail.com>
2024-09-27 09:28:35 +0700

    initial commit

---
+++ JavaIsland.java
@@ -0,0 +1,15 @@
+public class JavaIsland{
+
+    public static void main(String[] Args){
+
+        int x=2;
+        int y=3;
+
+        if (y>x)
+        
+        System.out.println("Hello!");
+         
+        else System.out.println("Goodbye.");
+    }
+}
+
